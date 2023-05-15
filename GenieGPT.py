d1aff01f38fe1b64d3b200f4ec8d6c6dbf74852a
Josh T <114734188+IkuStudies@users.noreply.github.com>
2023-05-15 14:50:20 -0500

    Update GenieGPT.py

--- GenieGPT.py
+++ GenieGPT.py
@@ -10,25 +10,9 @@ def home():
 @app.route('/start_chat', methods=['POST'])
 def start_chat():
     model_name = request.form['model']
-    subprocess.Popen(['python', f'{model_name}.py'])
+    subprocess.Popen(['python3', f'{model_name}.py'])
     return f'Chat with {model_name} started!'
 
 if __name__ == '__main__':
     app.run()
-from flask import Flask, render_template, request
-import subprocess
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/start_chat', methods=['POST'])
-def start_chat():
-    model_name = request.form['model']
-    subprocess.Popen(['python', f'{model_name}.py'])
-    return f'Chat with {model_name} started!'
-
-if __name__ == '__main__':
-    app.run()
